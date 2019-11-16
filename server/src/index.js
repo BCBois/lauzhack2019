bcbf3092a37b05dbb18c0c835ff04ce1b160c933
Zyf <zyfarok@gmail.com>
2019-11-16 14:29:18 +0100

    finish(?) simple chat server

--- server/src/index.js
+++ server/src/index.js
@@ -15,8 +15,10 @@ io.on('connection', function(socket){
     console.log('a user connected')
     socket.on('chat_message', function(msg){
         console.log('message: ' + msg)
+        io.emit('chat_message', msg)
     })
     socket.on('disconnect', function(){
       console.log('user disconnected')
     })
+    
 })
