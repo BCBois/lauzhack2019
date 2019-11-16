db862046afa417620b78884e5e84a5df5bb5ea41
Loïc Droz <lk.droz@gmail.com>
2019-11-16 22:56:07 +0100

    Set unique id in createThread

--- server/src/database.js
+++ server/src/database.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 
 const state = []
+var tid = 0
 
 module.exports = {
     getAllThreads: () => {
@@ -10,6 +11,8 @@ module.exports = {
     },
 
     createThread: (thread) => {
+        thread.id = tid
+        tid += 1
         state.push(thread)
     },
 
