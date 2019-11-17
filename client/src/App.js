245f040577db58f787522494380f0274d84d94b1
Alexandre Chau <accounts@chau.moe>
2019-11-17 09:50:20 +0100

    Add some styles, refactor Chat

--- client/src/App.js
+++ client/src/App.js
@@ -1,61 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import openSocket from 'socket.io-client'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCommentAlt, faCheckSquare, faDollarSign, faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons'
 
 import './App.css';
-import Thread from './Thread'
-import ThreadList from './ThreadList'
-import ThreadCreator from './ThreadCreator'
-
-const socket = openSocket('http://localhost:12345/');
+import Chat from './Chat'
 
 function App() {
-  const [view, setView] = useState("threadlist")
-  const [threadId, setThreadId] = useState("")
-  const [threadList, setThreadList] = useState([])
+  const [main, setMain] = useState("chat")
 
-  useEffect(() => {
-    socket.emit('get_all_threads', (data) => {
-      setThreadList(data)
-    })
-    socket.on('notify_new_thread', (thread) => {
-      setThreadList(prev => [...prev, thread])
-    })
-    socket.on('notify_new_msg', (threadId, msg) => {
-      setThreadList(prev =>
-        prev.map(thread => {
-          if (thread.id === threadId) {
-            return {
-              ...thread,
-              messages: [...thread.messages, msg]
-            }
-          } else {
-            return thread
-          }
-        })
-      )
-    })
-  }, [])
-
-  var inner
-  switch (view) {
-    case "threadlist":
-      inner = <ThreadList threadList={threadList} toThread={(threadId) => { setThreadId(threadId); setView("thread") }} toCreator={() => {setThreadId(null); setView("threadcreator")}} />
-      break;
-    case "thread":
-      inner = <Thread thread={threadList.filter(t => t.id === threadId)[0]} parentId={threadId} socket={socket} back={() => setView("threadlist")} subthread={() => setView("threadcreator")} />
-      break;
-    case "threadcreator":
-      inner = <ThreadCreator socket={socket} back={() => setView("threadlist")} />
-      break;
-    default:
-      inner = <p>Error : Wrong view name selected</p>
-      break;
+  var content
+  switch (main) {
+    case "chat": {
+      content = <Chat />
+    }
   }
 
   return (
     <div className="App">
-      <p>{view}</p>
-      {inner}
+      <div className="sidebar">
+        <FontAwesomeIcon icon={faCommentAlt} size="3x" fixedWidth />
+        <FontAwesomeIcon icon={faCheckSquare} size="3x" fixedWidth />
+        <FontAwesomeIcon icon={faDollarSign} size="3x" fixedWidth />
+        <FontAwesomeIcon icon={faMapMarkedAlt} size="3x" fixedWidth />
+      </div>
+      <div className="main">{content}</div>
     </div>
   )
 }
---
+++ client/src/Chat.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import openSocket from 'socket.io-client'
+import Thread from './Thread'
+import ThreadList from './ThreadList'
+import ThreadCreator from './ThreadCreator'
+
+const socket = openSocket('http://localhost:12345/');
+
+function Chat() {
+  const [view, setView] = useState("threadlist")
+  const [threadId, setThreadId] = useState("")
+  const [threadList, setThreadList] = useState([])
+
+  useEffect(() => {
+    socket.emit('get_all_threads', (data) => {
+      setThreadList(data)
+    })
+    socket.on('notify_new_thread', (thread) => {
+      setThreadList(prev => [...prev, thread])
+    })
+    socket.on('notify_new_msg', (threadId, msg) => {
+      setThreadList(prev =>
+        prev.map(thread => {
+          if (thread.id === threadId) {
+            return {
+              ...thread,
+              messages: [...thread.messages, msg]
+            }
+          } else {
+            return thread
+          }
+        })
+      )
+    })
+  }, [])
+
+  var inner
+  switch (view) {
+    case "threadlist":
+      inner = <ThreadList threadList={threadList} toThread={(threadId) => { setThreadId(threadId); setView("thread") }} toCreator={() => { setThreadId(null); setView("threadcreator") }} />
+      break;
+    case "thread":
+      inner = <Thread thread={threadList.filter(t => t.id === threadId)[0]} parentId={threadId} socket={socket} back={() => setView("threadlist")} subthread={() => setView("threadcreator")} />
+      break;
+    case "threadcreator":
+      inner = <ThreadCreator socket={socket} back={() => setView("threadlist")} />
+      break;
+    default:
+      inner = <p>Error : Wrong view name selected</p>
+      break;
+  }
+
+  return inner;
+}
+
+export default Chat;
